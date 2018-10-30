52d1e148422db22eba026648fba84870af268a44
jiyilanzhou <450595468@qq.com>
2018-10-30 15:19:54 +0800

    p2p/dial: add sleep time for task

--- p2p/dial.go
+++ p2p/dial.go
@@ -175,7 +175,7 @@ func (s *dialstate) newTasks(nRunning int, peers map[discover.NodeID]*Peer, now 
 		return true
 	}
 
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 2)
 
 	// Compute number of dynamic dials necessary at this point.
 	needDynDials := s.maxDynDials
